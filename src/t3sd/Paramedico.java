182a50f77c7ae985f7ff52d59b9d9cc893711ba7
Felipe F Valdivia <felipefvaldivia@Felipes-MacBook-Pro.local>
2018-12-12 12:14:11 -0300

    First commit

---
+++ src/t3sd/Paramedico.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t3sd;
+
+/**
+ *
+ * @author felipefvaldivia
+ */
+public class Paramedico {
+    int id;
+    String name;
+    String rut;
+    int age;
+    
+    public int edit_exam(){
+        return 1;
+        
+    }
+            
+}
