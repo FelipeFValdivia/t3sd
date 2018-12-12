a879c25d8c63eb95f8732d44a550297dadfd97c3
Miguel <migue.abaddonbboys@outlook.com>
2018-12-12 12:52:55 -0300

    doctor initial version

---
+++ src/t3sd/Doctor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t3sd;
+
+/**
+ *
+ * @author usuario
+ */
+public class Doctor {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int estudios;
+    private int experiencia;
+    
+    public Doctor(int id, String nombre, String apellido, int estudios, int experiencia){
+        id = id;
+        nombre = nombre;
+        apellido = apellido;
+        estudios = estudios;
+        experiencia = experiencia;
+    }
+    
+}
