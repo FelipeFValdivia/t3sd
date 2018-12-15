87683b9e0ab45eff35fb9da196ddb3e4e1215ef5
Felipe F Valdivia <felipefvaldivia@Felipes-MacBook-Pro.local>
2018-12-14 23:57:12 -0300

    Add parser of json

--- src/t3sd/T3SD.java
+++ src/t3sd/T3SD.java
@@ -6,11 +6,16 @@ package t3sd;
  * and open the template in the editor.
  */
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
  
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 /**
@@ -21,14 +26,27 @@ public class T3SD {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException {
         // TODO code application logic here
+        final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir);
         JSONParser parser = new JSONParser();
-        Object obj = parser.parse(new FileReader(
-        "personal.json")); 
-        JSONObject jsonObject = (JSONObject) obj;
-        println(jsonObject);
+        Object obj;
+        try { 
+            obj = parser.parse(new FileReader(
+                    "personal.json"));
+        JSONObject personal = (JSONObject) obj;
+        System.out.println(personal);            
+        } catch (IOException | ParseException ex) {
+            Logger.getLogger(T3SD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    private static void println(JSONObject jsonObject) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
